2874713b300e9e817bceba41e4897570eb97b526
zubair12147 <muhammadzubair121470@gmail.com>
2023-07-11 22:09:18 +0500

    Course Project completed without following any guidelines

--- app.js
+++ app.js
@@ -7,9 +7,43 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+let postHeading = [];
+let postContent = [];
+
 app.get('/', (req,res)=>{
-    res.render('index');
+    res.render('home',{mainTitle: "Personal Blog", post_headings: postHeading, post_contents: postContent});
 })
+
+app.get('/add_post', (req,res) =>{
+    res.render('add_post');
+});
+
+app.post('/', (req,res)=>{
+    var title = req.body.title;
+    var content = req.body.postBody;
+    if(title !== "" && content !== ""){
+        postHeading.push(title);
+        postContent.push(content);
+    }
+    res.redirect('/');
+})
+
+app.post('/display',(req,res) =>{
+    let heading = req.body.text;
+    res.render('post_page',{mainTitle: heading, heading:heading, content: postContent[postHeading.indexOf(heading)]});
+})
+
+app.get('/Home', (req,res) =>{
+    res.redirect('/');
+});
+
+app.get('/about', (req,res) =>{
+    res.render('about');
+});
+
+app.get('/contact', (req,res) =>{
+    res.render('contact');
+});
 
 app.listen(process.env.PORT || PORT,()=>{
     console.log(`server is running at port: ${PORT}`)
